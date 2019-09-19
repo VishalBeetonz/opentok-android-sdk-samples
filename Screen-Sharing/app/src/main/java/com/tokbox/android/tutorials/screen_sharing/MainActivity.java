aef387fbf868ae464894f97cd0f7acfc105f8b94
VishalBeetonz <53287764+VishalBeetonz@users.noreply.github.com>
2019-09-19 14:39:22 +0530

    Update MainActivity.java

--- Screen-Sharing/app/src/main/java/com/tokbox/android/tutorials/screen_sharing/MainActivity.java
+++ Screen-Sharing/app/src/main/java/com/tokbox/android/tutorials/screen_sharing/MainActivity.java
@@ -92,6 +92,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+      // this is for easily manage permission
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
